5f6ced9bb7fce739a87a1ed0a7372349aeb5c77b
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2024-01-09 19:27:18 +0530

    refactor: sidebar complete

--- src/components/templates/sidebar/sidebar.tsx
+++ src/components/templates/sidebar/sidebar.tsx
@@ -5,29 +5,41 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <section className="flex flex-col bg-[#1E2640]">
+    <section className="flex flex-col bg-[#1E2640] w-[224px] py-[16px] px-[10px] gap-[24px]">
       {/* User info */}
-      <article className="flex bg-red-300">
-        <Image
-          src={`/static_assests/images/Image.png`}
-          alt={`profile_pic`}
-          width={39}
-          height={39}
-        />
+      <article className="flex justify-between mx-[8px]">
+        <div className="flex gap-[12px]">
+          <Image
+            src={`/static_assests/images/Image.png`}
+            alt={`profile_pic`}
+            width={39}
+            height={39}
+          />
 
-        <div className="flex flex-col">
-          <p className="capitalize">{`nishyam`}</p>
-          <p className="text-white underline">{`Visit store`}</p>
+          <div className="flex flex-col">
+            <p className="text-white capitalize">{`nishyam`}</p>
+            <p className="text-white  underline">{`Visit store`}</p>
+          </div>
         </div>
+
+        <Image
+          src={`/static_assests/icons/sidebar/Chevron Down.svg`}
+          width={20}
+          height={20}
+          alt={``}
+        />
       </article>
 
       {/* sidebar links */}
-      <div className="flex-1 flex flex-col bg-green-400">
+      <div className="flex-1 flex flex-col gap-[4px]">
         {SIDEBAR_DATA.map((obj: ISidebar, idx: number) => {
           const { img_url, name } = obj;
           return (
             <React.Fragment key={idx}>
-              <div className="flex">
+              <div
+                className={` ${name == "Payouts" ? "bg-[#FFFFFF]/[0.10]" : ""}
+                flex px-[16px] py-[8px] gap-[12px] rounded-[4px] hover:bg-[#FFFFFF]/[0.10]`}
+              >
                 <Image
                   alt={`icon${idx}`}
                   src={img_url}
@@ -47,17 +59,19 @@ const Sidebar = () => {
       </div>
 
       {/* user credit */}
-      <article className="flex bg-pink-400">
-        <Image
-          alt={`wallet`}
-          src={`/static_assests/icons/sidebar/wallet.svg`}
-          width={24}
-          height={24}
-          unoptimized
-          priority
-          quality={90}
-          loading="eager"
-        />
+      <article className="flex bg-[#353C53] gap-[10px] rounded-[4px] items-center px-[12px] py-[6px]">
+        <div className="p-[4px] rounded-[4px] bg-[#FFFFFF]/[0.10]">
+          <Image
+            alt={`wallet`}
+            src={`/static_assests/icons/sidebar/wallet.svg`}
+            width={24}
+            height={24}
+            unoptimized
+            priority
+            quality={90}
+            loading="eager"
+          />
+        </div>
 
         <div className="flex flex-col text-white">
           <p>{`Available credits`}</p>
